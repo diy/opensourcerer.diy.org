2c5f5a96aeabf6c75f29fb5867567cf78da77ce8
jllord <jessica@codeforamerica.org>
2013-02-17 19:14:33 -0800

    try again

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path');
+var filed = require('filed');
+
+var htmldir = path.resolve(__dirname, 'attachments')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  filed(path.join(htmldir, 'index.html')).pipe(resp)
+  filed(path.join(htmldir, 'index.html')).pipe(response)
 });
 
 var port = process.env.PORT || 5000;
