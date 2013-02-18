53d9da0c550513515bb2586f40eb3a8182ed5eb1
jllord <jessica@codeforamerica.org>
2013-02-18 08:31:23 -0800

    woo

--- web.js
+++ web.js
@@ -1,15 +1,10 @@
 var connect = require('connect')
-var http = require('http');
+var path = require('path')
+var http = require('http')
+
+// var htmldir = path.resolve(__dirname, './')
 
 var app = connect()
-  .use(connect.favicon())
-  .use(connect.logger('dev'))
-  .use(connect.static('public'))
-  .use(connect.directory('public'))
-  .use(connect.cookieParser())
-  .use(connect.session({ secret: 'my secret here' }))
-  .use(function(req, res){
-    res.end('Hello from Connect!\n');
-  });
+  .use(connect.static('./'))
 
 http.createServer(app).listen(3000);
