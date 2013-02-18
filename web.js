14a20533e4da8ca040a7509c07232cc046262600
jllord <jessica@codeforamerica.org>
2013-02-17 19:53:14 -0800

    add

--- web.js
+++ web.js
@@ -1,20 +1,15 @@
-var path = require('path');
-var tako = require('tako')
-var filed = require('filed');
 var request = require('request').defaults({json: true})
-
 
 var htmldir = path.resolve(__dirname, 'attachments')
 
-var t = tako()
-
-t.route('/', function (req, resp) {
- filed(path.join(htmldir, 'index.html')).pipe(resp)
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+  }
 })
 
-t.route('/*').files(htmldir)
-
 var port = process.env.PORT || 5000;
-t.listen(port, function() {
+request.listen(port, function() {
   console.log("Listening on " + port);
 });	
