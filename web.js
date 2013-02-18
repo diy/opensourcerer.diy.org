ae34818e42275d30c519f833ca4e43c4986b1a92
jllord <jessica@codeforamerica.org>
2013-02-17 19:49:39 -0800

    add

--- web.js
+++ web.js
@@ -1,21 +1,20 @@
-var express = require('express');
 var path = require('path');
 var tako = require('tako')
 var filed = require('filed');
+var request = require('request').defaults({json: true})
+
 
 var htmldir = path.resolve(__dirname, 'attachments')
 
 var t = tako()
 
-var app = express.createServer(express.logger());
-
 t.route('/', function (req, resp) {
-  filed(path.join(htmldir, 'index.html')).pipe(resp)
+ filed(path.join(htmldir, 'index.html')).pipe(resp)
 })
 
 t.route('/*').files(htmldir)
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+t.listen(port, function() {
   console.log("Listening on " + port);
 });	
