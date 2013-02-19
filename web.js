467bb0a02222daf2c3228aff36e97574f1c1babe
jllord <jessica@codeforamerica.org>
2013-02-18 19:29:01 -0800

    woo

--- web.js
+++ web.js
@@ -1,13 +1,1 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.configure(function(){
-  server.use('/media', express.static(__dirname + '/media'));
-  server.use(express.static(__dirname + '/public'));
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+require('http').createServer(ecstatic('./attachments')).listen(5000)
