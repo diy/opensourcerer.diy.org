ab3953bdc18881e8aeb942c39cc39902d97d0bd1
jllord <jessica@codeforamerica.org>
2013-02-18 19:58:54 -0800

    back

--- web.js
+++ web.js
@@ -1,12 +1,2 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var ecstatic = require('ecstatic')
+var app = require('http').createServer(ecstatic('./')).listen(5000)
