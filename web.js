10a5444711ed2b68f4df8468015c062a00902eaf
jllord <jessica@codeforamerica.org>
2013-02-18 20:17:03 -0800

    added port to webjs

--- web.js
+++ web.js
@@ -1,2 +1,8 @@
 var ecstatic = require('ecstatic')
-var app = require('http').createServer(ecstatic('./')).listen(5000)
+
+var app = require('http').createServer(ecstatic('./'))
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
