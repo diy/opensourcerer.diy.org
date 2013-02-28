cc680be441c1b6c9dd3c3a5181ea77dd0db756ef
jllord <jessica@codeforamerica.org>
2013-02-27 23:02:53 -0800

    now with moar nodes

--- app.js
+++ app.js
@@ -18,16 +18,16 @@ var http    = require('http'),
 /**
  * Server
  */
-var config  = {
+var pageData  = {
     host:       process.env.HOST || argv.h,
     port:       Number(process.env.PORT) || argv.p,
     production: process.env.NODE_ENV === 'production'
 };
-var route   = require('./route.js')(router, config),
+var route   = require('./route.js')(router, pageData),
     app     = http.createServer(route);
 
 /**
  * Listen
  */
-app.listen(config.port);
-console.log('Listening at %s on port %d', config.host, config.port);
+app.listen(pageData.port);
+console.log('Listening at %s on port %d', pageData.host, pageData.port);
--- route.js
+++ route.js
@@ -9,17 +9,17 @@
  * Dependencies
  */
 var _           = require('underscore'),
+    $           = require('cheerio'),
     fs          = require('fs'),
     filed       = require('filed'),
     handlebars  = require('handlebars'),
-    markdown    = require('markdown').markdown;
-
-
+    markdown    = require('markdown').markdown,
+    lru         = require('lru-cache');          
 
 /**
  * Compile template
  */
-function compile (config) {
+function compileIndex (pageData) {
     // Markdown
     var raw         = fs.readFileSync(__dirname + '/installgit.md').toString();
     var content     = markdown.toHTML(raw);
@@ -29,12 +29,30 @@ function compile (config) {
     var template    = handlebars.compile(source);
 
     // Compose and return
-    var provider    = _.extend(config, {
+    var provider    = _.extend(pageData, {
         content: content
     });
     return template(provider);
 }
 
+/**
+ * Make a Page
+ */
+function makePage (pageData, num) {
+    // Markdown
+    var raw         = fs.readFileSync(__dirname + '/' + num + '.md').toString();  
+    var content     = markdown.toHTML(raw);  
+
+    // Handlebars
+    var source      = fs.readFileSync(__dirname + '/static/index.html').toString();
+    var template    = handlebars.compile(source);
+
+    // Compose and return
+    var provider    = _.extend(pageData, {
+        content: content
+    });
+    return template(provider);
+}
 
 /**
  * Static file server.
@@ -44,29 +62,52 @@ function compile (config) {
  *
  * @return {void}
  */
-function static (req, res) {
+function serveStatic (req, res) {
     filed(__dirname + '/static' + req.url).pipe(res);
+}
+
+function activeState (page, id) {
+    page('.c' + id).addClass('active')
 }
 
 /**
  * Export
  */
-module.exports = function (router, data) {
+module.exports = function (router, pageData) {
     // Init router
     var route = router();
+    var cache = lru()
 
     // Root
-    var root = compile(data);
-    route.get('/', function (req, res) {
-        res.writeHead(200);
-        res.end(root);
+    var root = compileIndex(pageData);
+    route.get('/', function (request, response) {
+        response.writeHead(200);
+        response.end(root);
     });
 
+route.get('/challenge/{num}', function(req, res) {
+    var num = req.params.num; // ex: if the path is /foo/bar, then base = foo
+    var cachePile = cache.get(num)
+    if (cachePile) {
+        res.writeHead(200)
+        res.end(cachePile)
+    }
+    else {
+        var page = makePage(pageData, num) 
+        var pageCheerio = $.load(page)
+        activeState(pageCheerio, num)
+        page = pageCheerio.html()
+        cache.set(num, page)
+        res.writeHead(200)
+        res.end(page)
+    }
+});
+
     // Static
-    route.get('/css/*', static);
-    route.get('/images/*', static);
-    route.get('/js/*', static);
-    route.get('/fonts/*', static);
+    route.get('/css/*', serveStatic);
+    route.get('/images/*', serveStatic);
+    route.get('/js/*', serveStatic);
+    route.get('/fonts/*', serveStatic);
 
     // Export
     return route;
